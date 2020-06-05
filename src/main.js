d3d668a342cd6852986240518f74e57b6f107215
极度呀 <2428292910@qq.com>
2020-06-05 17:46:18 +0800

    一次性提交

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import store from './store'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 //配置请求的跟路径
-axios.defaults.baseURL ='http://timemeetyou.com:8889/api/private/v1/'
-axios.interceptors.request.use(config =>{
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
   console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
@@ -16,6 +17,8 @@ axios.interceptors.request.use(config =>{
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -6,35 +6,51 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Cate from '../components//goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
+import Goods from '../components/goods/Goods.vue'
+import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
+
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', redirect: '/login'},
+    { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home,
-    //子路由
+    {
+      path: '/home', component: Home,
+      //子路由
       redirect: '/welcome',
-      children: [{path: '/welcome', component: Welcome},
-                  {path: '/users', component: Users},
-                  {path: '/rights', component: Rights},
-                  {path: '/roles', component: Roles}
-          ]
-   } 
+      children: [{ path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params },
+      { path: '/goods', component: Goods },
+      { path: '/goods/add', component: Add},
+      { path: '/orders', component: Order},
+      { path: '/reports', component: Report}
+
+
+      ]
+    }
   ]
 })
 
 //挂载路由导航守卫
-router.beforeEach((to,from,next) =>{
+router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from 代表从哪个路径跳转而来
   //next 是一个函数，表示放行
   // next（）放行 next（'/login'）强制跳转
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   //获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
